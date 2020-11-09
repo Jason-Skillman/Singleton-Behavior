dc7b72e5a3a17c41af540f26066450fb4883ae46
JasonSkillman <jason_skillman@outlook.com>
2020-11-09 18:40:46 -0500

    Refactor name and namespace

---
+++ Runtime/Scripts/SingletonBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Singleton {
+	public class SingletonBehavior<T> : MonoBehaviour where T : MonoBehaviour {
+
+		public static T Instance { get; private set; }
+
+		protected virtual void Awake() {
+			if(Instance == null) Instance = this as T;
+			else Destroy(gameObject);
+		}
+
+	}	
+}
