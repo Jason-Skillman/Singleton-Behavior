44cfd2e4faeeeddc76c374d5f2cf6e3199aa28fc
JasonSkillman <jason_skillman@outlook.com>
2020-11-09 18:23:28 -0500

    Create Singleton.cs

---
+++ Runtime/Scripts/Singleton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+	public static T Instance { get; private set; }
+
+	protected virtual void Awake() {
+		if(Instance == null) Instance = this as T;
+		else Destroy(gameObject);
+	}
+
+}
