e7d6d87f9e6236c639ea4c9587fafa66125fa74f
JasonSkillman <jason_skillman@outlook.com>
2020-12-20 21:46:33 -0500

    Update SingletonBehavior to be auto created

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Unity Singleton Changelog
 
+## [1.1.0] - 12/20/2020
+- Updated `SingltonBehavior` script to be auto created if one does not exist.
+- Added `DontDestroyOnLoad()`.
+
 ## [1.0.0] - 11/09/2020
 - Unity singlton release.
---
+++ Runtime/Scripts/Singleton/SingletonBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Singleton {
+	public class SingletonBehavior<T> : MonoBehaviour where T : MonoBehaviour {
+
+		private static T instance;
+
+		public static T Instance {
+			get {
+				if(instance == null) {
+					//Try to find an existing T object in scene
+					instance = FindObjectOfType<T>();
+					
+					//Create a new T GameObject if one does not exist
+					if(instance == null) {
+						GameObject emptyGameObject = new GameObject();
+						emptyGameObject.name = typeof(T).ToString();
+						instance = emptyGameObject.AddComponent<T>();
+					}
+				}
+				return instance;
+			}
+		}
+
+		protected virtual void Awake() {
+			if(Instance == null) {
+				Destroy(gameObject);
+				return;
+			}
+
+			DontDestroyOnLoad(gameObject);
+			
+			instance = GetComponent<T>();
+		}
+
+	}	
+}
